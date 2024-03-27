387e958efaee8a96d51db94afa35cac348c90a50
Adrià Chalaux <adriachalaux@gmail.com>
2024-03-27 12:06:23 +0100

    Some changes on the New Task Component

--- src/components/NewTaskComponent.vue
+++ src/components/NewTaskComponent.vue
@@ -6,16 +6,22 @@ const taskTitle = ref('')
 const tasksStore = useTasksStore()
 
 const _addTask = async () => {
-  if (taskTitle.value.trim()) {
-    await tasksStore.addNewTask(taskTitle.value)
+  // Eliminar espacios en blanco al principio y al final
+  const trimmedTitle = taskTitle.value.trim()
+
+  // Verificar que el título tenga al menos 4 caracteres / si no salta error de Supabase
+  if (trimmedTitle.length >= 4) {
+    await tasksStore.addNewTask(trimmedTitle)
     taskTitle.value = '' // Limpiar el input después de añadir la tarea
+  } else {
+    console.log('El título debe ser superior a 4 carácteres')
   }
 }
 </script>
 
 <template>
   <div>
-    <input v-model="taskTitle" placeholder="Nueva Tarea" />
+    <input v-model="taskTitle" @keyup.enter="_addTask" placeholder="Nueva Tarea" />
     <button @click="_addTask">Añadir Tarea</button>
   </div>
 </template>
